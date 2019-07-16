62fd394dd483e9c9d245d5833dc68833348f7ffb
Jeremy Coando <jaycoan@yahoo.com>
2019-07-15 18:27:29 -0600

    first commit terraform

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "AKIAXSI6HAOBHLKQW3FJ"
+  secret_key = "M5o+zqzLgeg+cYLEDoT97vnp2DnlgwTszhnmjxEL"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-0d4357dfb115cbeaa"
   instance_type = "t2.micro"
 }
